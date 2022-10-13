7d60f74668c3ab7ebaf111fa560518f48c79900b
kengo-k <kurobanekengo@gmail.com>
2022-10-13 22:33:28 +0900

    main: debugを追加してSQL文を確認できるようにする

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"sample-go-with-gorm.com/src/service"
@@ -13,19 +11,22 @@ func main() {
 	if err != nil {
 		panic("failed to connect database")
 	}
+	db = db.Debug()
 
 	service := &service.Service{}
-	orders := service.SelectAllOrders(db)
-	fmt.Printf("order count: %d\n", len(orders))
-	for _, order := range orders {
-		fmt.Printf("--------------\n")
-		fmt.Printf("  order id: %d\n", order.ID)
-		orderItems := order.OrderItems
-		fmt.Printf("    order items count: %d\n", len(orderItems))
-		for _, oi := range orderItems {
-			fmt.Printf("      item id: %d\n", oi.Item.ID)
-		}
-		cust := order.Customer
-		fmt.Printf("    customer id: %d\n", cust.ID)
-	}
+	/*orders := */ service.SelectAllOrders(db)
+	//service.SelectAllOrderItems(db)
+
+	// fmt.Printf("order count: %d\n", len(orders))
+	// for _, order := range orders {
+	// 	fmt.Printf("--------------\n")
+	// 	fmt.Printf("  order id: %d\n", order.ID)
+	// 	orderItems := order.OrderItems
+	// 	fmt.Printf("    order items count: %d\n", len(orderItems))
+	// 	for _, oi := range orderItems {
+	// 		fmt.Printf("      item id: %d\n", oi.Item.ID)
+	// 	}
+	// 	cust := order.Customer
+	// 	fmt.Printf("    customer id: %d\n", cust.ID)
+	// }
 }
