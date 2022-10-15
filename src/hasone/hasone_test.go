9b540bf5a430331b8f44eb6e02e8268be579a2a6
kengo-k <kurobanekengo@gmail.com>
2022-10-15 20:27:58 +0900

    add hasone test

--- src/hasone/hasone_test.go
+++ src/hasone/hasone_test.go
@@ -28,11 +28,8 @@ func initDB() {
 	}
 	cards := []*CreditCard{
 		{ID: 200, Number: "0000-0000-0001", UserID: 100},
-		{ID: 201, Number: "0000-0000-0002", UserID: 100},
-		{ID: 202, Number: "0000-0000-0003", UserID: 100},
-		{ID: 203, Number: "0000-0000-0004", UserID: 101},
-		{ID: 204, Number: "0000-0000-0005", UserID: 102},
-		{ID: 205, Number: "0000-0000-0006", UserID: 102},
+		{ID: 201, Number: "0000-0000-0002", UserID: 101},
+		{ID: 202, Number: "0000-0000-0003", UserID: 102},
 	}
 
 	for _, u := range users {
@@ -49,5 +46,51 @@ func TestMain(m *testing.M) {
 	m.Run()
 }
 
-func TestIt(t *testing.T) {
+func TestHasone(t *testing.T) {
+	var users []User
+	err := db.
+		Model(&User{}).
+		Preload("CreditCard").
+		Order("id asc").
+		Find(&users).
+		Error
+	if err != nil {
+		t.Errorf("err is not nil")
+	}
+
+	// test users len
+	gotUsersLen := len(users)
+	wantUsersLen := 3
+	if gotUsersLen != wantUsersLen {
+		t.Errorf("users len: got=%v, want=%v", gotUsersLen, wantUsersLen)
+	}
+
+	// test cards len
+	var cards []CreditCard
+	err = db.Model(&CreditCard{}).Find(&cards).Error
+	if err != nil {
+		t.Errorf("err is not nil")
+	}
+	gotCardsLen := len(cards)
+	wantCardsLen := 3
+	if gotCardsLen != wantCardsLen {
+		t.Errorf("cards len: got=%v, want=%v", gotCardsLen, wantCardsLen)
+	}
+
+	// test users and cards relations
+	user1 := users[0]
+	user1Card := user1.CreditCard
+	user2 := users[1]
+	user2Card := user2.CreditCard
+	user3 := users[2]
+	user3Card := user3.CreditCard
+
+	checkCard := func(got string, actual string) {
+		if got != actual {
+			t.Errorf("card: got=%v, actual=%v", got, actual)
+		}
+	}
+	checkCard(user1Card.Number, "0000-0000-0001")
+	checkCard(user2Card.Number, "0000-0000-0002")
+	checkCard(user3Card.Number, "0000-0000-0003")
 }
