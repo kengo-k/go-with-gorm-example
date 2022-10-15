60cf61e62c49101c3002a5dbc467d8ba36f7d996
kengo-k <kurobanekengo@gmail.com>
2022-10-15 19:58:35 +0900

    hasone: 初期データを投入

--- src/hasone/hasone_test.go
+++ src/hasone/hasone_test.go
@@ -20,9 +20,32 @@ type CreditCard struct {
 
 var db *gorm.DB
 
-func TestMain(m *testing.M) {
+func initDB() {
 	db = database.New("hasone.db")
 	db.AutoMigrate(&User{}, &CreditCard{})
+	users := []*User{
+		{ID: 100}, {ID: 101}, {ID: 102},
+	}
+	cards := []*CreditCard{
+		{ID: 200, Number: "0000-0000-0001", UserID: 100},
+		{ID: 201, Number: "0000-0000-0002", UserID: 100},
+		{ID: 202, Number: "0000-0000-0003", UserID: 100},
+		{ID: 203, Number: "0000-0000-0004", UserID: 101},
+		{ID: 204, Number: "0000-0000-0005", UserID: 102},
+		{ID: 205, Number: "0000-0000-0006", UserID: 102},
+	}
+
+	for _, u := range users {
+		db.Save(u)
+	}
+
+	for _, c := range cards {
+		db.Save(c)
+	}
+}
+
+func TestMain(m *testing.M) {
+	initDB()
 	m.Run()
 }
 
