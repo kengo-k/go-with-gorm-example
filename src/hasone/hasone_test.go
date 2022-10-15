37d3abb660909829a39670084c8fa016390eb160
kengo-k <kurobanekengo@gmail.com>
2022-10-15 19:15:37 +0900

    hasone確認用のソースをひとまずコミット

---
+++ src/hasone/hasone_test.go
@@ -0,0 +1,30 @@
+package hasone
+
+import (
+	"testing"
+
+	"gorm.io/gorm"
+	database "sample-go-with-gorm.com/src/db"
+)
+
+type User struct {
+	ID         uint
+	CreditCard CreditCard
+}
+
+type CreditCard struct {
+	ID     uint
+	Number string
+	UserID uint
+}
+
+var db *gorm.DB
+
+func TestMain(m *testing.M) {
+	db = database.New("hasone.db")
+	db.AutoMigrate(&User{}, &CreditCard{})
+	m.Run()
+}
+
+func TestIt(t *testing.T) {
+}
